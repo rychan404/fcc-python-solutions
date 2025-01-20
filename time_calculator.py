40c2f9c81be4b6ab102cfbda89aab02fb9419046
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-01-20 16:39:33 -0500

    Add period to split start time

--- time_calculator.py
+++ time_calculator.py
@@ -1,14 +1,18 @@
 def add_time(start, duration):
-    # Split start time into hours & mins
+    # Split start time into hours, mins, & period
     split = start.index(':')
     start_hours = ''
     start_mins = ''
+    start_period = ''
+
     for i in range(len(start)):
         if start[i].isdigit():
             if i < split:
                 start_hours += start[i]
             else:
                 start_mins += start[i]
+        elif start[i].isalpha():
+            start_period += start[i]
 
     # Split duration time into hours & mins
     split = duration.index(':')
@@ -21,10 +25,11 @@ def add_time(start, duration):
                 duration_hours += duration[i]
             else:
                 duration_mins += duration[i]
-
+    
     # Tests
     print(start_hours)
     print(start_mins)
+    print(start_period)
     print(duration_hours)
     print(duration_mins)
 
