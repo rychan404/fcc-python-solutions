4af9e29abec6b402c180c5d4104cdc199b31e8bc
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-01-20 15:36:45 -0500

    Split start time into hours & mins

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,21 @@
 def add_time(start, duration):
+    # Split start time into hours & mins
+    split = start.index(':')
+    hours = ''
+    mins = ''
+    for i in range(len(start)):
+        if start[i].isdigit():
+            if i < split:
+                hours += start[i]
+            else:
+                mins += start[i]
 
+    # Tests
+    print(hours)
+    print(mins)
 
-
-
-
+    new_time = start
     return new_time
+
+# Tests
+print(add_time('3:00 PM', '3:10'))
