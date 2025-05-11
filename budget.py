a6a5b36c3c774d8ee00e227b5fecb68ecae3d404
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-05-11 13:12:55 -0400

    Modify __init__

--- budget.py
+++ budget.py
@@ -5,6 +5,3 @@ class Category:
 
     def deposit(self, amount, description=""):
         self.ledger.append({'amount': amount, 'description': description})
-
-def create_spend_chart(categories):
-    pass
