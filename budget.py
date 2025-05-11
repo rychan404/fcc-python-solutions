8806e5a8cab18e4ec94b48580c531c1bd1ee6a62
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-05-11 16:45:10 -0400

    Add withdraw

--- budget.py
+++ budget.py
@@ -1,7 +1,13 @@
 class Category:
     def __init__(self, name, ledger):
         self.name = name
-        self.ledger = ledger
+        self.ledger = list()
 
     def deposit(self, amount, description=""):
         self.ledger.append({'amount': amount, 'description': description})
+
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount):
+            self.ledger.append({'amount' : amount, 'description' : description})
+            return True
+        return False
