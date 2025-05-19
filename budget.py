7290b4a7c950cf3b8a1ed4cb306b41a70ec11c68
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-05-18 22:12:08 -0400

    Add __str__ (only title)

--- budget.py
+++ budget.py
@@ -11,12 +11,14 @@ class Category:
             self.ledger.append({'amount' : -amount, 'description' : description})
             return True
         return False
+    
     def transfer(self, amount, category):
         if self.check_funds(amount):
             self.withdraw(amount, "Transfer to " + category.name)
             self.deposit(amount, "Transfer from " + self.name)
             return True
         return False
+    
     def get_balance(self):
         total = 0
         for process in self.ledger:
@@ -24,7 +26,16 @@ class Category:
                 if key == 'amount':
                     total += value
         return total
+    
     def check_funds(self, amount):
         if amount > self.get_balance():
             return False
         return True
+    
+    def __str__(self):
+        title = ''
+        for _ in range((30 - len(self.name)) / 2):
+            title += '*'
+        title += self.name
+        for _ in range(30 - len(title)):
+            title += '*'
