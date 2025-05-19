c0ab3865dd56495c20e49f0c60f62a1279e254d0
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-05-18 22:15:26 -0400

    Add tests, fixed __init__ / __str__

--- budget.py
+++ budget.py
@@ -1,5 +1,5 @@
 class Category:
-    def __init__(self, name, ledger):
+    def __init__(self, name):
         self.name = name
         self.ledger = list()
 
@@ -34,8 +34,17 @@ class Category:
     
     def __str__(self):
         title = ''
-        for _ in range((30 - len(self.name)) / 2):
+        for _ in range(int((30 - len(self.name)) / 2)):
             title += '*'
         title += self.name
         for _ in range(30 - len(title)):
             title += '*'
+        return title
+
+food = Category('Food')
+food.deposit(1000, 'deposit')
+food.withdraw(10.15, 'groceries')
+food.withdraw(15.89, 'restaurant and more food for dessert')
+clothing = Category('Clothing')
+food.transfer(50, clothing)
+print(food)
