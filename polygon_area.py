43de40cf4b90a68dcf56f430c9a98960023e2ba3
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-06-11 10:07:01 -0400

    Add getter methods

--- polygon_area.py
+++ polygon_area.py
@@ -9,5 +9,15 @@ class Rectangle:
     def set_height(self, height):
         self.height = height
 
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+    
+    def get_diagonal(self):
+        return (self.width**2 + self.height**2)**0.5
+    
+    
 class Square(Rectangle):
     pass
