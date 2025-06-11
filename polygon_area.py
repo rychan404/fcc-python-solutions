9d5359188c43f8b41426306872b10674f6922891
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-06-11 12:25:38 -0400

    Add setter methods to Square class

--- polygon_area.py
+++ polygon_area.py
@@ -43,6 +43,14 @@ class Square(Rectangle):
     def __init__(self, side):
         super().__init__(side, side)
     
+    def set_width(self, side):
+        super().set_width(side)
+        super().set_height(side)
+    
+    def set_height(self, side):
+        super().set_width(side)
+        super().set_height(side)
+    
     def set_side(self, side):
         super().set_width(side)
         super().set_height(side)
