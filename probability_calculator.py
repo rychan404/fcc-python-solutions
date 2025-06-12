8a2ae9aa38a6b0775a1470827ddf12d50bd030b3
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-06-12 18:25:59 -0400

    Add draw

--- probability_calculator.py
+++ probability_calculator.py
@@ -3,11 +3,20 @@ import random
 
 class Hat:
     def __init__(self, **kwargs):
-        contents = []
+        self.contents = []
         for color, number in kwargs.items():
             for _ in range(number):
-                contents.append(color)
-        print(contents)
+                self.contents.append(color)
+    
+    def draw(self, number):
+        if number > len(self.contents):
+            return self.contents
+        for _ in range(number):
+            self.contents.pop(random.randint(0, len(self.contents) - 1))
+        return self.contents
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
+
+hat1 = Hat(yellow=3, blue=2, green=6)
+print(hat1.draw(3))
